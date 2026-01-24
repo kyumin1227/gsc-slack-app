cffd47b161c8cdc536221cc7bb2d8bdcf4902c35
kyumin1227 <kyumin12271227@gmail.com>
2026-01-24 22:21:39 +0900

    Setting: 슬랙 앱 연결 및 상호작용 확인
    - 슬랙 앱 개발 라이브러리 설치
    - Test 슬랙앱 연결 확인

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Event, Message } from 'nestjs-slack-bolt';
+import type { AllMiddlewareArgs, SlackEventMiddlewareArgs } from '@slack/bolt';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,45 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Message('hi')
+  message({ say }: SlackEventMiddlewareArgs<'message'>) {
+    say('Hello');
+  }
+
+  @Event('app_home_opened')
+  async event({
+    client,
+    event,
+    logger,
+  }: SlackEventMiddlewareArgs<'app_home_opened'> & AllMiddlewareArgs) {
+    try {
+      const result = await client.views.publish({
+        user_id: event.user,
+        view: {
+          // Home tabs must be enabled in your app configuration page under "App Home"
+          type: 'home',
+          blocks: [
+            {
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                text: '*Welcome home, <@' + event.user + '> :house:*',
+              },
+            },
+            {
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                text: 'Learn how home tabs can be more useful and interactive <https://docs.slack.dev/surfaces/app-home|*in the documentation*>.',
+              },
+            },
+          ],
+        },
+      });
+      logger.info(result);
+      logger.info(event);
+    } catch (error) {}
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SlackModule } from 'nestjs-slack-bolt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    SlackModule.forRoot({
+      token: process.env.SLACK_BOT_TOKEN,
+      socketMode: true,
+      appToken: process.env.SLACK_APP_TOKEN,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
