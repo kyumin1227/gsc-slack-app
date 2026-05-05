a178ac4a2f2eacb1a438791983c9c74c68ab612d
MINGYUKIM <coinmagic8085@gmail.com>
2026-05-05 13:00:53 +0900

    refactor: Slack Bolt 구조에 맞춰 REST생성물 제거

--- src/cleaning/cleaning.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CleaningService } from './cleaning.service';
-import { CleaningController } from './cleaning.controller';
-
-@Module({
-  controllers: [CleaningController],
-  providers: [CleaningService],
-})
-export class CleaningModule {}
--- src/cleaning/dto/update-cleaning.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCleaningDto } from './create-cleaning.dto';
-
-export class UpdateCleaningDto extends PartialType(CreateCleaningDto) {}
