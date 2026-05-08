72e8a1c2745b08d99e6778f94df637dd3a924278
kyumin1227 <kyumin12271227@gmail.com>
2026-05-08 14:34:44 +0900

    refactor(resource): View 파일 분리 및 공통 상수 이동
    - resource.view.ts를 resource의 각 타입별 view 로 분리하여 재작성
    - Google calendar의 색상을 정하던 ROOM_COLORS 는 schedule 에서의 재사용을 염두하여 common의 constant로 분리

---
+++ src/common/constants.ts
@@ -0,0 +1,11 @@
+// Google Calendar 임베드 URL에서 사용하는 색상 팔레트 (URL 인코딩된 hex 값)
+export const CALENDAR_COLORS = [
+  '%234285F4',
+  '%23DB4437',
+  '%230F9D58',
+  '%23F4B400',
+  '%239E69AF',
+  '%23F6511D',
+  '%2300BCD4',
+  '%23E91E63',
+];
